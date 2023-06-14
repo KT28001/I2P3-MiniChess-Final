b28d2c940f2fca0fa44425ed3b54abc9ac7ba441
KT28001 <sfhezi0108@gmail.com>
2023-06-14 23:26:01 +0800

    alphabeta can now pass all the 2 baselines

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-./build/main ./build/player_alphabeta ./build/player_random
+./build/main ./baselines/linux/baseline1 ./build/player_alphabeta 
+
+#./build/player_random
+#./build/player_minimax 
